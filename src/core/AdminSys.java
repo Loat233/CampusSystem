2fb8540f2607518991c44daf8a5e7e40124e31c8
Loat233 <a2847894378@outlook.com>
2026-04-10 23:24:45 +0800

    Moved the files

--- src/core/AdminSys.java
+++ src/core/AdminSys.java
@@ -1,3 +1,5 @@
+package core;
+
 import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 
--- src/core/UserSys.java
+++ src/core/UserSys.java
@@ -1,3 +1,5 @@
+package core;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
